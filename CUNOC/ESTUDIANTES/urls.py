9a79fd15e89d6ae9576a6b8c35756dea779d1a6a
Henry-USAC <2995863750101@ingenieria.usac.edu.gt>
2023-10-23 02:01:17 -0600

    Datos no perdidos

--- CUNOC/ESTUDIANTES/urls.py
+++ CUNOC/ESTUDIANTES/urls.py
@@ -6,15 +6,13 @@ urlpatterns = [
 
     
     path('productos/', views.products, name='productos'),
-    path('logout/', views.exit, name='exit'),
+    path('logout/', views.salir, name='salir'),
     
     
     path('home/', views.home, name='home'),
     path('pensum/', views.Pensum, name='Pensum'), 
     path('registro/', VRegistro.as_view(), name="registro"),
-    path('LoginEstudiantes/', views.iniciar_sesion_estudiantes, name='InicioE'),
     path('PortalEstudiantes/', views.PE, name='PE'),
-    path('PortalEstudiantes/', views.PE, name='perfil'),
-    path('PortalEstudiantes/', views.PE, name='closed'),
+    path('LoginEstudiantes/', views.iniciar_sesion_estudiantes, name='InicioE')
 
 ]
--- CUNOC/ESTUDIANTES/views.py
+++ CUNOC/ESTUDIANTES/views.py
@@ -1,37 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import Group
 from django.views.generic import View
 from .forms import CustomUserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import allusuarios
 from django.contrib import messages
-# Create your views here.
-
-
-
 
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def Pensum(request):
     return render(request, 'pensum.html')
 
 def PE(request):
-    return render(request, 'PortalEstudiantes.html')
-
+    cliente_username = request.session.get('cliente_username', None)
+    if cliente_username is not None:
+        cliente = allusuarios.objects.get(username=cliente_username)
+        return render(request, "PortalEstudiantes.html", {"cliente": cliente})
+    else:
+        return redirect('InicioE')
 
 @login_required
 def products(request):
     return render(request, 'productos.html')
 
+def salir(request):
+    logout(request)
+    return redirect('home')
 
 def exit(request):
     logout(request)
     return redirect('home')
-
 
 def iniciar_sesion_estudiantes(request):
     if request.method == "POST":
@@ -39,26 +41,22 @@ def iniciar_sesion_estudiantes(request):
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            usuario = authenticate(
-                request=request, username=username, password=password)
+            usuario = authenticate(request=request, username=username, password=password)
             grupo_estudiantes = Group.objects.get(name='Estudiantes')
-
             if usuario is not None:
                 if not usuario.is_superuser and grupo_estudiantes in usuario.groups.all():
                     login(request, usuario)
-                    # Asegúrate de que esta función exista y sea necesaria
-                    # reset(username=username)
-                    return render(request, "PortalEstudiantes.html", {"form": form, "cliente": allusuarios.objects.get(username=username)})
+                    cliente = allusuarios.objects.get(username=username)
+                    request.session['cliente_username'] = cliente.username
+                    return render(request, "PortalEstudiantes.html", {"form": form, "cliente": cliente})
                 else:
                     messages.error(request, "Credenciales inválidas")
             else:
                 messages.error(request, "Usuario no válido")
         else:
             messages.error(request, "Información no válida")
-
     form = AuthenticationForm()
     return render(request, "LoginEstudiantes.html", {"form": form})
-
 
 class VRegistro(View):
     def get(self, request):
@@ -67,16 +65,13 @@ class VRegistro(View):
 
     def post(self, request):
         form = CustomUserCreationForm(request.POST, request.FILES)
-        print(form)
-
         if form.is_valid():
             usuario = form.save()
             ncui = form.cleaned_data.get('cui')
             img = form.cleaned_data.get("profile_imagen")
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-            usuario = authenticate(
-                request=request, username=username, password=password)
+            usuario = authenticate(request=request, username=username, password=password)
             login(request, usuario)
             nuevo_usuario = allusuarios(user=request.user, username=request.user.username, first_name=request.user.first_name,
                                         last_name=request.user.last_name, email=request.user.email, cui=ncui, profile_image=img)
