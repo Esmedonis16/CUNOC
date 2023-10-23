86b10e9be839b30a03bf8da56ea02e81d228a05a
Henry-USAC <2995863750101@ingenieria.usac.edu.gt>
2023-10-22 21:35:22 -0600

    >:/

--- CUNOC/ESTUDIANTES/apps.py
+++ CUNOC/ESTUDIANTES/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
-class EstudiantesConfig(AppConfig):
+class ESTUDIANTESConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ESTUDIANTES'
+    verbose_name = 'Registros de Perfiles'
+    def ready(self):
+        import ESTUDIANTES.signals
+
+    
