7e99f0a3c6279c53e4e2156985a989f414a00cb3
Esmeralda Donis <3607761400101@ingenieria.usac.edu.gt>
2023-10-26 16:44:14 -0600

    Restablecer contraseña

--- CUNOC/Admin_y_Docentes/urls.py
+++ CUNOC/Admin_y_Docentes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from . import views
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import views as auth_views
 from .views import RDocentes
 from django.conf.urls.static import static
 from django.conf import settings
@@ -9,8 +10,12 @@ from django.conf import settings
 urlpatterns = [
     path('registrodocente/', RDocentes.as_view(), name='signup'),
     path('boton/<int:pk>/', views.boton, name='boton'),
-    #path('mis_cursos/', views.cursos_del_estudiante, name='cursos_del_estudiante'),
-    #path('mis_cursos/', views.pensum, name='llamandocursos'),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset_password/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password/password_reset_complete.html"), name="password_reset_complete"),
+    
 
 ]
 if settings.DEBUG:
