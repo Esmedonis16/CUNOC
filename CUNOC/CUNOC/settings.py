cdae1c026eb932a615a4c068a5aa65bff8078f98
Esmeralda Donis <3607761400101@ingenieria.usac.edu.gt>
2023-11-08 19:43:55 -0600

    gitignoreeee

--- CUNOC/CUNOC/settings.py
+++ CUNOC/CUNOC/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-import CUNOC.db as db
+import CUNOC.config as config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,7 +177,7 @@ WSGI_APPLICATION = 'CUNOC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = db.POSTGRESQL
+DATABASES = config.POSTGRESQL
 
 
 # Password validation
